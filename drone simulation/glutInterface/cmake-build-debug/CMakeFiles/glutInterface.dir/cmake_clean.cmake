c387513ad60b5f2d536839ceeeee6a0428f3ee79
GMFVF2000 <58947214+GMFVF2000@users.noreply.github.com>
2020-01-31 23:23:45 +0100

    Add files via upload

---
+++ drone simulation/glutInterface/cmake-build-debug/CMakeFiles/glutInterface.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/glutInterface.dir/glutWindow.cpp.o"
+  "libglutInterface.pdb"
+  "libglutInterface.so"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/glutInterface.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
